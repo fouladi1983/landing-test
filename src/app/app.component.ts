b4ceaffa5b4b9b509e3328c961a2d9f11176bd42
Mohammad <fouladi1983@gmail.com>
2020-07-14 22:28:50 +0430

    starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'landing-test';
+  stickyHeader: boolean = false;
+  constructor() { }
+
+  @HostListener('window:scroll', ['$event']) 
+  scroll = (event) => {
+    if(window.pageYOffset > 600){
+      this.stickyHeader = true;
+      console.log(`it's fixed`);
+      console.log(this.stickyHeader);
+    }else{
+      this.stickyHeader = false;
+      console.log(this.stickyHeader);
+    }
+      
+  }
+
 }
